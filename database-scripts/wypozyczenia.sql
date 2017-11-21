eee8c58fa08a577a6fd270d103f73a32cb6a282c
Maksymilian Maczurek <maksiu96@windowslive.com>
2017-11-21 14:47:42 +0100

    updated *.sql files

---
+++ database-scripts/wypozyczenia.sql
@@ -0,0 +1,14 @@
+INSERT INTO wypozyczenia (KlientID, GraPlanszowaID, DataWypozyczenia, DataZwrotu, Status)
+VALUES ('218', '3', '17-11-17', '17-11-20', 'Zrealizowane');
+
+INSERT INTO wypozyczenia (KlientID, GraPlanszowaID, DataWypozyczenia, DataZwrotu, Status)
+VALUES ('218', '25', '17-11-21', null , 'Do odbioru');
+
+INSERT INTO wypozyczenia (KlientID, GraPlanszowaID, DataWypozyczenia, DataZwrotu, Status)
+VALUES ('123', '75', '17-09-05', null, 'Anulowane');
+
+
+SELECT * FROM wypozyczenia;
+
+
+
---
+++ database-scripts/zamowieniaWewnetrzne.sql
@@ -0,0 +1,28 @@
+INSERT INTO zamowieniawewnetrzne (KlientID, DataZlozenia, Status)
+VALUES ('13', '15-11-20', 'Zrealizowane');
+
+INSERT INTO zamowieniawewnetrzne (KlientID, DataZlozenia, Status)
+VALUES ('654', '16-09-25', 'Zrealizowane');
+
+INSERT INTO zamowieniawewnetrzne (KlientID, DataZlozenia, Status)
+VALUES ('500', '17-10-07', 'Anulowane');
+
+INSERT INTO zamowieniawewnetrzne (KlientID, DataZlozenia, Status)
+VALUES ('13', '16-09-11', 'Zrealizowane');
+
+INSERT INTO zamowieniawewnetrzne (KlientID, DataZlozenia, Status)
+VALUES ('429', '16-01-03', 'Zrealizowane');
+
+INSERT INTO zamowieniawewnetrzne (KlientID, DataZlozenia, Status)
+VALUES ('500', '17-10-07', 'Do odbioru');
+
+INSERT INTO zamowieniawewnetrzne (KlientID, DataZlozenia, Status)
+VALUES ('423', '17-11-21', 'Do odbioru');
+
+INSERT INTO zamowieniawewnetrzne (KlientID, DataZlozenia, Status)
+VALUES ('965', '17-11-21', 'Do odbioru');
+
+INSERT INTO zamowieniawewnetrzne (KlientID, DataZlozenia, Status)
+VALUES ('14', '17-11-17', 'Do odbioru');
+
+SELECT * FROM zamowieniawewnetrzne;
